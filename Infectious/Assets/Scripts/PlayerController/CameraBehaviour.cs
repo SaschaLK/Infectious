db46aef7f21280b009a36f6c777f2982a16c870a
SaschaLK <SaschaL.Kufahl@gmail.com>
2019-03-20 23:53:18 +0100

    Camera spawn and lock on, Work in Progress - Next task camera movement

---
+++ Infectious/Assets/Scripts/PlayerController/CameraBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    public GameObject world;
+    public float distance;
+    private Vector3 position = Vector3.up;
+
+    private void Start() {
+        float sphereBuffer = world.GetComponent<SphereMapGenerator>().distanceBetweenElements;
+        position = position * sphereBuffer * distance;
+        transform.position = position;
+    }
+
+    private void Update() {
+        transform.LookAt(world.transform);
+    }
+}
