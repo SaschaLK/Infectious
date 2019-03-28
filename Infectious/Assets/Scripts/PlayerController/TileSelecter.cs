cf85ccb09791c4cea3fff04f3de7bf748dd21c32
SaschaLK <SaschaL.Kufahl@gmail.com>
2019-03-28 15:45:04 +0100

    Tile Selection work in Progress

---
+++ Infectious/Assets/Scripts/PlayerController/TileSelecter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSelecter : MonoBehaviour {
+
+    private Camera playerCamera;
+    private RaycastHit hit;
+    private Ray ray;
+
+    private void Start() {
+        playerCamera = GetComponent<Camera>();
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+            Debug.Log(Input.mousePosition);
+        }
+    }
+}
