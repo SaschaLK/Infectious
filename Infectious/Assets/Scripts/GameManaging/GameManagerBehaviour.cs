7836b2eac8931fb333aca4ea70234365a8fa42bd
SaschaLK <SaschaL.Kufahl@gmail.com>
2019-04-02 01:14:51 +0200

    Main Menu and Main Scene change loop with esc Menu V1

---
+++ Infectious/Assets/Scripts/GameManaging/GameManagerBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerBehaviour : MonoBehaviour {
+
+    public static GameManagerBehaviour instance;
+    public GameObject quickMenu;
+
+    private void Start() {
+        quickMenu.SetActive(false);
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Cancel") && !quickMenu.activeInHierarchy) {
+            quickMenu.SetActive(true);
+        }
+        else if(Input.GetButtonDown("Cancel") && quickMenu.activeInHierarchy) {
+            quickMenu.SetActive(false);
+        }
+    }
+
+    public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+}
