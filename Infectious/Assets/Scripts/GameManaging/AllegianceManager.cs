3f9375f4a2ac0ed61cd2fade07a91517f56138b0
SaschaLK <SaschaL.Kufahl@gmail.com>
2019-04-24 16:39:27 +0200

    Added Game State Manager

--- Infectious/Assets/Scripts/GameManaging/AllegianceManager.cs
+++ Infectious/Assets/Scripts/GameManaging/AllegianceManager.cs
@@ -9,7 +9,6 @@ public class AllegianceManager : MonoBehaviour {
     public float transitionTime;
     public Color playerPartyColor;
     public int setLocationCharges = 1;
-    public bool gameRunning;
 
     private void Awake() {
         instance = this;
---
+++ Infectious/Assets/Scripts/GameManaging/GameStateManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour {
+    public static GameStateManager instance;
+
+    public bool gameRunning = true;
+
+    private void Awake() {
+        instance = this;
+    }
+}
