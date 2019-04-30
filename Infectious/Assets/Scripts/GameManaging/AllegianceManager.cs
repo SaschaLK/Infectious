10a4a23209b86cde8ed6623854d6c5cabaec20c6
SaschaLK <SaschaL.Kufahl@gmail.com>
2019-04-30 18:35:21 +0200

    Clearing Infection V1

--- Infectious/Assets/Scripts/GameManaging/AllegianceManager.cs
+++ Infectious/Assets/Scripts/GameManaging/AllegianceManager.cs
@@ -6,10 +6,11 @@ public class AllegianceManager : MonoBehaviour {
 
     public static AllegianceManager instance;
 
-    [Range(0.1f, 1f)]
+    [Range(1, 100)]
     public float infectionThreshold = 0.8f;
     public Color playerPartyColor;
     public int setLocationCharges = 1;
+    public int allegianceFactorAccuracy = 100;
 
     private void Awake() {
         instance = this;
--- Infectious/Assets/Scripts/Tiles/Allegiance.cs
+++ Infectious/Assets/Scripts/Tiles/Allegiance.cs
@@ -16,6 +16,7 @@ public class Allegiance : MonoBehaviour {
 
     //States and required Information for calculations
     private float allegianceFactor;
+    private int allegianceFactorAccuracy;
     private float baseColorValue;
     private float playerPartyColorValue;
     private bool transitioning;
@@ -34,6 +35,7 @@ public class Allegiance : MonoBehaviour {
         //Manager informations
         playerPartyColor = AllegianceManager.instance.playerPartyColor;
         infectionThreshold = AllegianceManager.instance.infectionThreshold;
+        allegianceFactorAccuracy = AllegianceManager.instance.allegianceFactorAccuracy;
 
         //Local variables and calculation requirements
         baseColorValue = (baseColor.r + baseColor.g + baseColor.b) / 3;
@@ -59,26 +61,52 @@ public class Allegiance : MonoBehaviour {
     private IEnumerator Transition() {
         transitioning = true;
         while (!hasTransitioned) {
+            #region Setup transition information
+            //Defines if currently growing or shrinking
             float speed = (PointsManager.instance.unifiedPointsFactor - 1) * Time.deltaTime;
-            mat.color = new Color(Mathf.Lerp(mat.color.r, playerPartyColor.r, speed), Mathf.Lerp(mat.color.g, playerPartyColor.g, speed), Mathf.Lerp(mat.color.b, playerPartyColor.b, speed));
-            allegianceFactor = (mat.color.r + mat.color.g + mat.color.b) / 3;
-            allegianceFactor = (allegianceFactor - baseColorValue) / (playerPartyColorValue - baseColorValue);
-            if(allegianceFactor > infectionThreshold && !thresholdReached) {
-                Debug.Log("infect");
+
+            //If growing or shrinking, set the color accordingly on object
+            if (speed > 0) {
+                mat.color = new Color(Mathf.Lerp(mat.color.r, playerPartyColor.r, speed), Mathf.Lerp(mat.color.g, playerPartyColor.g, speed), Mathf.Lerp(mat.color.b, playerPartyColor.b, speed));
+            }
+            else if(speed < 0) {
+                mat.color = new Color(Mathf.Lerp(mat.color.r, baseColor.r, speed * - 1), Mathf.Lerp(mat.color.g, baseColor.g, speed * - 1), Mathf.Lerp(mat.color.b, baseColor.b, speed * - 1));
+            }
+
+            //Set the current allegianceFactor, floored or ceiled according to if growing or shrinking
+            float tempAllegiance = (((mat.color.r + mat.color.g + mat.color.b) / 3) - baseColorValue) / (playerPartyColorValue - baseColorValue);
+            if(speed > 0) {
+                allegianceFactor = Mathf.Ceil(tempAllegiance * allegianceFactorAccuracy);
+            }
+            else {
+                allegianceFactor = Mathf.Floor(tempAllegiance * allegianceFactorAccuracy);
+            }
+            #endregion
+
+            #region Logic according to information in Setup transition information
+            //Current Logic enables the spreading of infestation before maximum threshold is reached
+
+            //If threshold reached, spread infection
+            if (allegianceFactor >= infectionThreshold && !thresholdReached) {
                 thresholdReached = true;
                 GetComponent<WorldTileBehaviour>().InfestNeighbours();
             }
-            if(allegianceFactor >= 1) {
-                Debug.Log("Stop trans");
+
+            //If currently shrinking and no infection, clear neighbours and stop routine
+            if(speed < 0 && allegianceFactor <= 0) {
+                allegianceFactor = 0;
+                GetComponent<WorldTileBehaviour>().ClearNeighbours();
+                StopAllCoroutines();
+            }
+
+            //If allegianceFactor reached the ceiling set by allegianceFactorAccuracy (10, 100, 1000, ...) -> stop routine
+            if(allegianceFactor >= allegianceFactorAccuracy) {
                 hasTransitioned = true;
                 transitioning = false;
                 StopAllCoroutines();
             }
-            //else if (allegianceFactor <= 0 && !thresholdReached) {
-            //    Debug.Log("Start clear");
-            //    thresholdReached = true;
-            //    GetComponent<WorldTileBehaviour>().ClearNeighbours();
-            //}
+            #endregion
+
             yield return new WaitForFixedUpdate();
         }
     }
